108bbb071db41fcc62af95b846f360e44aecfbf3
Aleksei <osipov.rb@gmail.com>
2022-04-24 09:51:09 +0300

    реализовал команду /top

--- commands/DickCommand.php
+++ commands/DickCommand.php
@@ -22,39 +22,43 @@ class DickCommand extends UserCommand
 
         $chatid = $message->getChat()->getId();
         $userid = $message->getFrom()->getId();
-        $username = $message->getFrom()->getUsername();
+        $mention = $message->getFrom()->tryMention();
 
         $stmt = $pdo->prepare('SELECT * FROM plays WHERE userid = :userid AND chatid = :chatid AND playdate = CURDATE()');
         $stmt->execute(compact('userid', 'chatid'));
         if ($stmt->fetchColumn()) { // сегодня уже играл
+            // --- ВРЕМЕННОЕ
+            $stmt = $pdo->prepare('UPDATE dicks SET mention = :mention WHERE userid = :userid AND chatid = :chatid');
+            $stmt->execute(compact('mention', 'userid', 'chatid'));
+            // --- END ВРЕМЕННОЕ
             Carbon::setLocale('ru');
             $comeAfter = Carbon::now()->endOfDay()->longRelativeToNowDiffForHumans($parts = 2);
-            return $this->replyToChat('@'.$username.', ты уже играл сегодня. Приходи ' . $comeAfter);
+            return $this->replyToChat($mention.', ты уже играл сегодня. Приходи ' . $comeAfter);
         } else {
             $stmt = $pdo->prepare('SELECT dicklen FROM dicks WHERE userid = :userid AND chatid = :chatid LIMIT 1');
             $stmt->execute(compact('userid', 'chatid'));
             $dicklen = $stmt->fetchColumn();
 
             if (!$dicklen) {
-                $stmt = $pdo->prepare('INSERT INTO dicks VALUES (:userid, :chatid, "0")');
-                $stmt->execute(compact('userid', 'chatid'));
                 $dicklen = 0;
+                $stmt = $pdo->prepare('INSERT INTO dicks VALUES (:userid, :chatid, :dicklen, :mention)');
+                $stmt->execute(compact('userid', 'chatid', 'dicklen', 'mention'));
             }
         
             do {
                 $delta_plus = random_int(1, 10) > 2;
                 $delta_dicklen = $delta_plus ? random_int(1, 10) : random_int(-10, -1);
                 $new_dicklen = $dicklen + $delta_dicklen;
-            } while ($new_dicklen < 0);            
+            } while ($new_dicklen < 0);
 
             $new_dicklen_msg = $delta_plus
                 ? 'вырос на ' . $delta_dicklen. ' см.'
                 : 'сократился на ' . $delta_dicklen . ' см.';
 
-            $stmt = $pdo->prepare('UPDATE dicks SET dicklen = :new_dicklen WHERE userid = :userid AND chatid = :chatid');
-            $stmt->execute(compact('new_dicklen', 'userid', 'chatid'));
+            $stmt = $pdo->prepare('UPDATE dicks SET dicklen = :new_dicklen, mention = :mention WHERE userid = :userid AND chatid = :chatid');
+            $stmt->execute(compact('new_dicklen', 'mention', 'userid', 'chatid'));
 
-            $message = '@' . $username . ', твой член ' . $new_dicklen_msg 
+            $message = $mention . ', твой член ' . $new_dicklen_msg 
                 . ' Теперь его длина: ' . $new_dicklen . ' см.';
 
             $stmt = $pdo->prepare('INSERT INTO plays VALUES (:userid, :chatid, CURDATE())');
--- commands/TopCommand.php
+++ commands/TopCommand.php
@@ -5,6 +5,7 @@ namespace Longman\TelegramBot\Commands\UserCommands;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Entities\User;
 
 class TopCommand extends UserCommand
 {
@@ -15,6 +16,19 @@ class TopCommand extends UserCommand
 
     public function execute(): ServerResponse
     {
-        return $this->replyToChat('Пока не реализовано, сорян. Скоро доделаю...');
+        $pdo = \Longman\TelegramBot\DB::getPdo();
+
+        $message = $this->getMessage();
+
+        $chatid = $message->getChat()->getId();
+        $stmt = $pdo->prepare('SELECT mention, dicklen FROM dicks WHERE chatid = :chatid ORDER BY dicklen DESC');
+        $stmt->execute(compact('chatid'));
+        $top10 = $stmt->fetchAll();
+
+        $message = 'Топ 10 хуеносцев:'.PHP_EOL.array_reduce($top10, fn($carry, $v) => $carry 
+            . (empty($v['mention']) ? 'Неизвестный шакал' : $v['mention']) . ': ' . $v['dicklen'] . ' см.' . PHP_EOL
+        );
+
+        return $this->replyToChat($message);
     }
 }
