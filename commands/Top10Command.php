7b5d1123a71ba56a32b3f44b8e5f354b99d7e174
Aleksei <osipov.rb@gmail.com>
2022-05-09 15:14:21 +0300

    доработки топ10

--- commands/Top10Command.php
+++ commands/Top10Command.php
@@ -25,12 +25,31 @@ class Top10Command extends UserCommand
         $stmt->execute(compact('chatid'));
         $top10 = $stmt->fetchAll();
 
-        $message = $top10
-            ?   'Топ 10 хуеносцев:' . PHP_EOL . array_reduce($top10, fn($carry, $v) => $carry 
-                    . (empty($v['mention']) ? 'Неизвестный шакал' : $v['mention']) . ': ' . $v['dicklen'] . ' см.' . PHP_EOL
-                )
-            :   'В этом чате писюны не найдены. Отправь /dick в чат чтобы вырастить член.';
+        if ($top10) {
+            $top10 = $this->makePlayersMentionTagless($top10);
+            $message = 'Топ 10 хуеносцев: ' . PHP_EOL . $this->makePlayersList($top10);
+        } else {
+            $message = 'В этом чате писюны не найдены. Отправь /dick в чат чтобы вырастить член.';
+        }
 
         return $this->replyToChat($message);
+    }
+
+    private function makePlayersMentionTagless($players) {
+        foreach ($players as &$player) { 
+            if (preg_match('/^@/', $player['mention'])) {
+                $player['mention'] = substr($player['mention'], 1);
+            }
+        }
+        
+        return $players;
+    }
+
+    private function makePlayersList($players) {
+        $place = 0;
+        return array_reduce($players, function($carry, $v) use (&$place) {
+            $place++;
+            return $carry . $place . '. ' .$v['mention'] . ': ' . $v['dicklen'] . ' см.' . PHP_EOL;
+        });
     }
 }
