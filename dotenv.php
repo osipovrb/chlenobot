da9b71a7acac63d3346696e097f5d3c8fbd49878
Aleksei <osipov.rb@gmail.com>
2022-05-12 09:30:52 +0300

    дополнил обязательные .env параметры

--- dotenv.php
+++ dotenv.php
@@ -3,4 +3,12 @@
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$dotenv->required(['BOT_API_KEY', 'BOT_USERNAME', 'BOT_HOOK_URL']);
+$dotenv->required([
+    'BOT_API_KEY', 
+    'BOT_USERNAME', 
+    'BOT_HOOK_URL', 
+    'MYSQL_HOST', 
+    'MYSQL_USERNAME', 
+    'MYSQL_PASSWORD', 
+    'MYSQL_DATABASE'
+]);
